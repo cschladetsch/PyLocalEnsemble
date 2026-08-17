cf8dd9c373805478199a1fb754e50827675576e6
Christian <christian.schladetsch@gmail.com>
2026-08-18 06:21:57 +1000

    CleN

---
+++ main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+consteval int square(int x) {
+    if (x < 0) throw "x must be non-negative";
+    return x * x;
+}
+
+int main() {
+    constexpr auto val = square(5);
+    std::cout << "Square: " << val << std::endl;
+    return 0;
+}
